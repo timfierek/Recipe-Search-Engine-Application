fe6e1e23f5c4ccdc5d69ef59512fe7b2cbc9da39
David <brushdm@gmail.com>
2022-08-01 20:14:42 -0400

    Added Service class and update Repo class.

---
+++ src/main/java/Service/RecipeService.java
@@ -0,0 +1,34 @@
+package Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import co.grandcircus.recipeapiproj.model.Recipe;
+
+@Service
+public class RecipeService {
+	
+	@Value("${recipe.apikey}")
+	private String apiKey;
+	
+	RestTemplate restTemplate = new RestTemplate(); 
+	
+	public Recipe getRecipeById(String id) {
+		String url = "https://api.spoonacular.com/recipes/" + id + "/information&apiKey=" + apiKey;
+		return restTemplate.getForObject(url, Recipe.class);
+	}
+	
+	public List<Recipe> getRecipeByIngredient(List<String> ingredients) {
+		String url = "https://api.spoonacular.com/recipes/complexSearch/?includeIngredients=" + ingredients +"&apiKey=" + apiKey;
+		return restTemplate.getForObject(url, RecipeResponse.class).getRecipes();
+	}
+	
+	public List<Recipe> getRecipeByName(String name) {
+		String url = "https://api.spoonacular.com/recipes/complexSearch/?titleMatch=" + name +"&apiKey=" + apiKey;
+		return restTemplate.getForObject(url, RecipeResponse.class).getRecipes();
+	}
+
+}
--- src/main/java/co/grandcircus/recipeapiproj/repository/RecipeRepository.java
+++ src/main/java/co/grandcircus/recipeapiproj/repository/RecipeRepository.java
@@ -1,9 +1,17 @@
 package co.grandcircus.recipeapiproj.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import co.grandcircus.recipeapiproj.model.Recipe;
 
 public interface RecipeRepository extends MongoRepository<Recipe, String>{
+	
+	List<Recipe> findAll();
+	
+	Optional<Recipe> findById(String id);
+	
 
 }
